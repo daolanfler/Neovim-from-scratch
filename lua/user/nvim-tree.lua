86e0a1f4ec945425f9e9427d2dc10b235e6b49e5
daolanfler <daolanfler@gmail.com>
2022-07-01 17:38:05 +0800

    chore: update config

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -26,11 +26,11 @@ nvim_tree.setup {
   },
   open_on_tab = false,
   hijack_cursor = false,
-  update_cwd = true,
-  update_to_buf_dir = {
+  hijack_directories = {
     enable = true,
     auto_open = true,
   },
+  update_cwd = true,
   diagnostics = {
     enable = true,
     icons = {
@@ -63,7 +63,6 @@ nvim_tree.setup {
     height = 30,
     hide_root_folder = false,
     side = "left",
-    auto_resize = true,
     mappings = {
       custom_only = false,
       list = {
@@ -92,6 +91,7 @@ nvim_tree.setup {
   },
   actions = {
     open_file = {
+      resize_window = true,
       quit_on_open = false,
       window_picker = {
         enable = true
