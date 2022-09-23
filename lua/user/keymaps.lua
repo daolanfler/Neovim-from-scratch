5e90825e37798273969f90b9a27a2f3d4bbf6977
daolanfler <daolanfler@gmail.com>
2022-09-23 13:36:04 +0800

    keymaps change

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -39,6 +39,14 @@ keymap("n", "<S-h>", ":bprevious<CR>", opts)
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 
+-- Better copy paste
+keymap("n", "<leader>x", "\"_x", opts)
+keymap("n", "<leader>s", "\"_s", opts)
+keymap("n", "<leader>d", "\"_d", opts)
+keymap("n", "<leader>c", "\"_c", opts)
+keymap("n", "<leader>y", "\"+y", opts)
+keymap("n", "<leader>y", "\"+y", opts)
+
 -- Insert --
 -- Press jk fast to enter
 keymap("i", "jk", "<ESC>", opts)
@@ -48,10 +56,16 @@ keymap("i", "jk", "<ESC>", opts)
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
+-- Better copy paste
+keymap("v", "<leader>d", "\"_d", opts)
+keymap("v", "<leader>c", "\"_c", opts)
+keymap("v", "<leader>p", "\"_dP", opts)
+keymap("v", "<leader>y", "\"+y", opts)
+-- keymap("v", "p", '"_dP', opts)
+
 -- Move text up and down
 keymap("v", "<A-j>", ":m .+1<CR>==", opts)
 keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
