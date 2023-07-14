3fe7f40f1d42ccaac7976edde9623e06b114e485
daolanfler <daolanfler@gmail.com>
2023-07-14 22:55:27 +0800

    update NvimTree config

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -47,15 +47,15 @@ vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
 
 -- for wsl clipboard
-vim.g.clipboard = {
-  name = 'WslClipboard',
-  copy = {
-    ["+"] = "clip.exe",
-    ["*"] = "clip.exe"
-  },
-  paste = {
-    ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))'
-  },
-  cache_enabled = 0
-}
+--[[ vim.g.clipboard = { ]]
+--[[   name = 'WslClipboard', ]]
+--[[   copy = { ]]
+--[[     ["+"] = "clip.exe", ]]
+--[[     ["*"] = "clip.exe" ]]
+--[[   }, ]]
+--[[   paste = { ]]
+--[[     ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))', ]]
+--[[     ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))' ]]
+--[[   }, ]]
+--[[   cache_enabled = 0 ]]
+--[[ } ]]
