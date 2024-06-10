97788328565cf094735e69bd9eac75563c9899a2
qifang <daolanfler@gmail.com>
2024-06-10 12:44:59 +0800

    chore: upgrade

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,9 +2,9 @@ return {
   'nvim-treesitter/nvim-treesitter',
   config = function()
     require 'nvim-treesitter'.setup({
-      ensure_installed = { "lua", "bash", "rust", "javascript", "typescript", "vue" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-      sync_install = false,                                                            -- install languages synchronously (only applied to `ensure_installed`)
-      ignore_install = { "" },                                                         -- List of parsers to ignore installing
+      ensure_installed = { "lua", "bash", "rust", "javascript", "typescript", "vue", "go" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+      sync_install = false,                                                                  -- install languages synchronously (only applied to `ensure_installed`)
+      ignore_install = { "" },                                                               -- List of parsers to ignore installing
       autopairs = {
         enable = true,
       },
