d26e0e3eca2cdb6c139e3c252d6e5da0e893df48
daolanfler <daolanfler@gmail.com>
2025-03-31 22:49:00 +0800

    chore: update

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,8 +2,11 @@ return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
 	cmd = "Neotree",
-	dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim"
+	},
 	keys = { {
 		"<leader>e",
 		"<cmd>Neotree toggle<cr>",
