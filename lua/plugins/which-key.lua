f48ccfdca15d94ab2333cdfdb2be5a3432e982ca
qifang <daolanfler@gmail.com>
2025-08-31 16:45:29 +0800

    which-key not auto-open

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,6 +5,8 @@ return {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
+		triggers = {} , -- 禁用自动弹出
+
 	},
 	keys = {
 		{
